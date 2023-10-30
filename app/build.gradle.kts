5a554aee5058bc5e2ecec40372de7eddd463a340
misha <misha@ninesquares.studio>
2023-10-30 21:06:59 +0200

    Display a scrollable list activity5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
