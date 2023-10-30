38392f1a71f20dfd57b04f4dfbd39edbc9e1236b
misha <misha@ninesquares.studio>
2023-10-30 21:14:45 +0200

    Display a scrollable list activity5.10

--- app/src/test/java/com/example/affirmations20/AffirmationsAdapterTests.kt
+++ app/src/test/java/com/example/affirmations20/AffirmationsAdapterTests.kt
@@ -1,4 +1,22 @@
 package com.example.affirmations20
 
+import ItemAdapter
+import com.example.affirmations20.model.Affirmation
+import org.junit.Test
+import android.content.Context
+import org.junit.Assert.assertEquals
+import org.mockito.Mockito.mock
 class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
 }
