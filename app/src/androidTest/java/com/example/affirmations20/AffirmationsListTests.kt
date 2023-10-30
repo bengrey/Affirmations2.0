c323978e2c1328ec988c0d38a8fcfceb94c4dc95
misha <misha@ninesquares.studio>
2023-10-30 21:10:04 +0200

    Display a scrollable list activity5.7

--- app/src/androidTest/java/com/example/affirmations20/AffirmationsListTests.kt
+++ app/src/androidTest/java/com/example/affirmations20/AffirmationsListTests.kt
@@ -1,5 +1,12 @@
 package com.example.affirmations20
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -8,11 +15,18 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AffirmationsListTests {
+
     @get:Rule
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun scroll_to_item() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
 
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
     }
 }
